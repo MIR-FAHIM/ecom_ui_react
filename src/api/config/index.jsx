e3ef53096494fd24f594c72592da21a39ae6d7a3
MirFahim <mirfahim@aapbd.com>
2026-04-12 10:26:19 +0600

    new

--- src/api/config/index.jsx
+++ src/api/config/index.jsx
@@ -5,10 +5,10 @@ export const google_map_key = "AIzaSyBgU4tHGpYmeb5KYEB9Ml4qoz1w-JJmKb8";
 //Ecom Vendor
 
 
-export const base_url = "https://ecomvendor.braintodo.com/public";
-export const image_file_url = "https://ecomvendor.braintodo.com/storage/app/public";
-export const appname = "Ecommerce Vendor App";
-export const siteName = "Ecommerce Vendor";
+export const base_url = "https://myzooapi.myzoo.asia/public";
+export const image_file_url = "https://myzooapi.myzoo.asia/storage/app/public";
+export const appname = "MyZoo";
+export const siteName = "MyZoo";
 export const companyID = "3";
 
 //jashore bro
