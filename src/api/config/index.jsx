93a0522af3b70760a8d42dfcc295d1927e13270f
MIR-FAHIM <ridoyfahim92@gmail.com>
2026-04-21 14:29:34 +0600

    new

--- src/api/config/index.jsx
+++ src/api/config/index.jsx
@@ -1,19 +1,16 @@
 //export const base_url = "http://127.0.0.1:8000";
 export const google_map_key = "AIzaSyBgU4tHGpYmeb5KYEB9Ml4qoz1w-JJmKb8";
 
-
 //Ecom Vendor
-
-
-export const base_url = "https://myzooapi.myzoo.asia/public";
-export const image_file_url = "https://myzooapi.myzoo.asia/storage/app/public";
-export const appname = "MyZoo";
-export const siteName = "MyZoo";
-export const companyID = "3";
+// export const base_url = "https://myzooapi.myzoo.asia/public";
+// export const image_file_url = "https://myzooapi.myzoo.asia/storage/app/public";
+// export const appname = "MyZoo";
+// export const siteName = "MyZoo";
+// export const companyID = "3";
 
 //jashore bro
-// export const base_url = "https://api.jashorebro.com/public";
-// export const image_file_url = "https://api.jashorebro.com/storage/app/public";
-// export const appname = "Ecommerce Vendor App";
-// export const siteName = "Ecommerce Vendor";
-// export const companyID = "3";
+export const base_url = "https://ecomvendor.braintodo.com/public";
+export const image_file_url = "https://ecomvendor.braintodo.com/storage/app/public";
+export const appname = "Ecommerce Vendor App";
+export const siteName = "Ecommerce Vendor";
+export const companyID = "3";
