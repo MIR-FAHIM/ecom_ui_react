d8e583e9afbe220f01c4665eb0b937e278391bb7
MIR-FAHIM <ridoyfahim92@gmail.com>
2026-06-21 16:55:21 +0600

    new

--- src/api/config/index.jsx
+++ src/api/config/index.jsx
@@ -9,8 +9,8 @@ export const google_map_key = "AIzaSyBgU4tHGpYmeb5KYEB9Ml4qoz1w-JJmKb8";
 // export const companyID = "3";
 
 //jashore bro
-export const base_url = "https://ecomvendor.braintodo.com/public";
-export const image_file_url = "https://ecomvendor.braintodo.com/storage/app/public";
+export const base_url = "https://myzooapi.myzoo.asia/public";
+export const image_file_url = "https://myzooapi.myzoo.asia/storage/app/public";
 export const appname = "Ecommerce Vendor App";
 export const siteName = "Ecommerce Vendor";
 export const companyID = "3";
